84af7857fb346d184b38f8a36d0ceda3b4cf1acf
Quintin Dunn <quintindunn67@gmail.com>
2023-03-01 12:26:34 -0500

    Refractored code

---
+++ config.py
@@ -0,0 +1,23 @@
+on_member_join_channel: int = 0
+guild_id: int = 0
+
+smart_switch_id: int = 0
+role_name: str = ""
+
+client_token: str = ""
+
+dad_jokes = [
+    "Why do fathers take an extra pair of socks when they go golfing?\nIn case they get a hole in one!",
+    "What do you call a fish wearing a bowtie?\nSofishticated.",
+    "I'm afraid for the calendar. Its days are numbered.",
+    "Dear Math, grow up and solve your own problems.",
+    "What did Baby Corn say to Mama Corn?\nWhere's Pop Corn?"
+]
+
+quotes = [
+    f"That guy above me is a goat",
+    f"私はペドファイルです",
+    f"All animals are equal, just some are more equal than others",
+    f"このなめるつま先の下の人",
+    f"69"
+]
